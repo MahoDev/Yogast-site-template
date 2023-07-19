94d8c4f94e5b7b9d84947d7948c306eeb0e1d383
MahoDev <mt.alshahat@gmail.com>
2023-07-19 16:43:17 +0300

    Made the slideshow code reusable

--- main.js
+++ main.js
@@ -1,3 +1,112 @@
+////reusable code start////
+
+//slidshow code start//
+function addSideItemAtDirection(directon, allItems, activeItem) {
+  const activeItemIndex = allItems.indexOf(activeItem);
+  let newItemToAdd = null;
+  if (directon === "left") {
+    let newItemIndex = activeItemIndex - 1;
+    /*if we've reached the end of element in left
+    direction, then go to the end of the loop in
+    the right direction
+    */
+    if (newItemIndex < 0) {
+      newItemIndex = allItems.length - 1;
+    }
+    newItemToAdd = allItems[newItemIndex];
+    //make sure there are no placement classe before adding
+    //fixes bug when going through items from end
+    newItemToAdd.style.visibility = "hidden";
+    newItemToAdd.classList.remove("right");
+    newItemToAdd.classList.add("left");
+    newItemToAdd.style.visibility = "visible";
+  } else {
+    let newItemIndex = activeItemIndex + 1;
+    /*if we've reached the end of element in right
+    direction, then go to the start of the loop in
+    the left direction
+    */
+    if (newItemIndex === allItems.length) {
+      newItemIndex = 0;
+    }
+    newItemToAdd = allItems[newItemIndex];
+    //make sure there are no placement classes before adding.
+    //fixes bug when going through items from start
+    newItemToAdd.style.visibility = "hidden";
+    newItemToAdd.classList.remove("left");
+    newItemToAdd.classList.add("right");
+    newItemToAdd.style.visibility = "visible";
+  }
+  activeItem.parentElement.insertBefore(
+    newItemToAdd,
+    activeItem.parentElement.lastElementChild //.right-arr element
+  );
+
+  return newItemToAdd;
+}
+
+// when i click an arrow, the current shown item moves to the
+// oposite direction of the arrow and the left/right item
+// replaces its position
+
+function moveActiveItemToDirection(activeItem, direction) {
+  activeItem.classList.replace("active", direction);
+}
+
+//makes the item on left or right active.
+//i.e it moves it from its direction to the center of the
+//slideshow by removing the displacement class left/right
+function makeSideItemActive(sideItem, direction) {
+  sideItem.classList.replace(direction, "active");
+}
+
+async function leftSlide(allItems, activeItem) {
+  if (!isItemSliding) {
+    preventClick();
+    let addedItem = addSideItemAtDirection("left", allItems, activeItem);
+    moveActiveItemToDirection(activeItem, "right");
+    // give time for the browser to register the property change
+    // for the animation to work.
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 50);
+    });
+    makeSideItemActive(addedItem, "left");
+  }
+}
+
+async function rightSlide(allItems, activeItem) {
+  if (!isItemSliding) {
+    preventClick();
+    let addedItem = addSideItemAtDirection("right", allItems, activeItem);
+    moveActiveItemToDirection(activeItem, "left");
+    // give time for the browser to register the property change
+    // for the animation to work.
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 50);
+    });
+
+    makeSideItemActive(addedItem, "right");
+  }
+}
+
+let isItemSliding = false;
+async function preventClick() {
+  isItemSliding = true;
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 800);
+  });
+  isItemSliding = false;
+}
+//slidshow code start end//
+
+////reusable code end////
+
 //Sidebar code start//
 const menuIcon = document.querySelector(".icons .menu-icon");
 const sidebar = document.querySelector(".sidebar");
@@ -10,127 +119,38 @@ closeIcon.onclick = () => {
 };
 //Sidebar code end//
 
-//Slideshow code start//
+//Hero section slideshow code start//
+const heroSlideshow = document.querySelector(".hero-section .slideshow");
+const heroLeftArr = document.querySelector(".hero-section .left-arr");
+const heroRightArr = document.querySelector(".hero-section  .right-arr");
 
-const slideshow = document.querySelector(".slideshow");
-const leftArr = document.querySelector(".left-arr");
-const rightArr = document.querySelector(".right-arr");
+const heroItem1 = document.querySelector(".hero-section .slideshow .item");
+const heroItem2 = heroItem1.cloneNode(true);
+const heroItem3 = heroItem1.cloneNode(true);
+heroItem2.querySelector("img").src = "imgs/hero section/football.png";
+heroItem2.querySelector("img").alt = "football";
+heroItem2.querySelector("img").title = "Photo by bella lac on Unsplash";
+heroItem3.querySelector("img").src = "imgs/hero section/volleyball.png";
+heroItem3.querySelector("img").alt = "volleyball";
+heroItem3.querySelector("img").title = "";
 
-const item1 = document.querySelector(".slideshow .item");
-const item2 = item1.cloneNode(true);
-const item3 = item1.cloneNode(true);
-item2.querySelector("img").src = "imgs/hero section/football.png";
-item2.querySelector("img").alt = "football";
-item2.querySelector("img").title = "Photo by bella lac on Unsplash";
-item3.querySelector("img").src = "imgs/hero section/volleyball.png";
-item3.querySelector("img").alt = "volleyball";
-item3.querySelector("img").title = "";
+//added later to avoid being cloned
+heroItem1.classList.add("active");
+let heroSectionItems = [heroItem1, heroItem2, heroItem3];
 
-item1.classList.add("active");
-let items = [item1, item2, item3];
-
-// when i click an arrow, the current shown item moves to the
-// oposite direction of the arrow and the left/right item
-// replaces its position
-
-function moveActiveItemToDirection(direction) {
-  let activeItem = document.querySelector(".slideshow .item.active");
-  activeItem.classList.replace("active", direction);
-}
-
-function addSideItemAtDirection(directon) {
-  let activeItem = document.querySelector(".slideshow .item.active");
-  const activeItemIndex = items.indexOf(activeItem);
-  let newItemToAdd = null;
-  if (directon === "left") {
-    let newItemIndex = activeItemIndex - 1;
-    /*if we've reached the end of element in left
-    direction, then go to the end of the loop in
-    the right direction
-    */
-    if (newItemIndex < 0) {
-      newItemIndex = items.length - 1;
-    }
-    newItemToAdd = items[newItemIndex];
-    //make sure there are no placement classe before adding
-    //fixes bug when going through items from end
-    newItemToAdd.style.visibility = "hidden";
-    newItemToAdd.classList.remove("right");
-    newItemToAdd.classList.add("left");
-    newItemToAdd.style.visibility = "visible";
-  } else {
-    let newItemIndex = activeItemIndex + 1;
-    /*if we've reached the end of element in right
-    direction, then go to the end of the loop in
-    the left direction
-    */
-    if (newItemIndex === items.length) {
-      newItemIndex = 0;
-    }
-    newItemToAdd = items[newItemIndex];
-    //make sure there are no placement classes before adding
-    //fixes bug when going through items from start
-    newItemToAdd.style.visibility = "hidden";
-    newItemToAdd.classList.remove("left");
-    newItemToAdd.classList.add("right");
-    newItemToAdd.style.visibility = "visible";
-  }
-  slideshow.insertBefore(
-    newItemToAdd,
-    document.querySelector(".hero-section .right-arr")
+heroLeftArr.onclick = async () => {
+  leftSlide(
+    heroSectionItems,
+    document.querySelector(".hero-section .item.active")
   );
-
-  return newItemToAdd;
-}
-//makes the item on left or right active.
-//i.e it moves it from its direction to the center of the
-//slideshow by removing the displacement class left/right
-function makeSideItemActive(item, direction) {
-  item.classList.replace(direction, "active");
-}
-
-let isItemSliding = false;
-leftArr.onclick = async () => {
-  if (!isItemSliding) {
-    preventClick();
-    let addedItem = addSideItemAtDirection("left");
-    moveActiveItemToDirection("right");
-    // give time for the browser to register the property change
-    // for the animation to work.
-    await new Promise((resolve) => {
-      setTimeout(() => {
-        resolve();
-      }, 50);
-    });
-    makeSideItemActive(addedItem, "left");
-  }
 };
-rightArr.onclick = async () => {
-  if (!isItemSliding) {
-    preventClick();
-    let addedItem = addSideItemAtDirection("right");
-    moveActiveItemToDirection("left");
-    // give time for the browser to register the property change
-    // for the animation to work.
-    await new Promise((resolve) => {
-      setTimeout(() => {
-        resolve();
-      }, 50);
-    });
-
-    makeSideItemActive(addedItem, "right");
-  }
+heroRightArr.onclick = async () => {
+  rightSlide(
+    heroSectionItems,
+    document.querySelector(".hero-section .item.active")
+  );
 };
 
-async function preventClick() {
-  isItemSliding = true;
-  await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, 500);
-  });
-  isItemSliding = false;
-}
 //Slideshow code end//
 
 // about section start//
@@ -155,4 +175,8 @@ playBtn.onclick = () => {
   }
 };
 
-// about section end//
+//about section end//
+
+//trainer section start//
+
+//trainer section end//
